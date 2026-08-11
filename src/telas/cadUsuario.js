42d2f20b02257e74f69cb68cb684cd0240c0cce3
Gislaine07 <gislainefranca1403@gmail.com>
2026-08-10 22:43:56 -0300

    atividade 10/08
    aula api expo

---
+++ src/telas/cadUsuario.js
@@ -0,0 +1,11 @@
+ 
+import { View, Text } from 'react-native'; 
+
+
+export default function CadUsuario(){
+    return (
+        <View>
+            <Text>CadUsusario</Text>
+        </View>
+    );
+}
---
+++ src/telas/recSenha.js
@@ -0,0 +1,11 @@
+ 
+import { View, Text } from 'react-native'; 
+
+
+export default function RecSenha(){
+    return (
+        <View>
+            <Text>RecSenha</Text>
+        </View>
+    );
+}
